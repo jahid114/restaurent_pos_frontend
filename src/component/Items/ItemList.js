ad1e93709f49531f68a8b387093185df82234412
jahid114 <jahidctgcse@gmail.com>
2022-11-03 13:04:41 +0600

    added itemlist

--- src/component/Items/ItemList.js
+++ src/component/Items/ItemList.js
@@ -1,4 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/UseAuth';
+import config from '../../config';
+
 const Itemlist = () => {
+  const navigate = useNavigate();
+  const authentication = useAuth();
+  const [itemList, setItemsList] = useState();
+
+  const processItemList = (response, navigate, setAuth, setItemsList) => {
+    console.log(response);
+    if (response.status === 200) {
+      setItemsList(response.data.items);
+    } else {
+      localStorage.clear();
+      setAuth();
+      navigate('/login');
+    }
+  };
+
+  useEffect(() => {
+    fetch(config.apiurl + '/items', {
+      headers: {
+        Authorization: 'Bearer ' + authentication.auth.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        processItemList(res, navigate, authentication.setAuthr, setItemsList);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className='container first'>
       <table className='table table-bordered'>
@@ -25,20 +58,23 @@ const Itemlist = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th className='text-center' scope='row'>
-              1
-            </th>
-            <td className='text-center'>Chicken chap</td>
-            <td className='text-center'>Bengali</td>
-            <td className='text-center'>150</td>
-            <td className='text-center'>
-              <i class='bi bi-pencil-square icon' style={{ color: '#594F8D' }}></i>
-            </td>
-            <td className='text-center'>
-              <i className='bi bi-trash icon'></i>
-            </td>
-          </tr>
+          {itemList &&
+            itemList.map((item) => (
+              <tr key={item.id}>
+                <th className='text-center' scope='row'>
+                  {item.id}
+                </th>
+                <td className='text-center'>{item.name}</td>
+                <td className='text-center'>{item.catagory}</td>
+                <td className='text-center'>{item.price}</td>
+                <td className='text-center'>
+                  <i class='bi bi-pencil-square icon' style={{ color: '#594F8D' }}></i>
+                </td>
+                <td className='text-center'>
+                  <i className='bi bi-trash icon'></i>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -6,34 +6,34 @@ const getToken = () => {
   const tokenString = localStorage.getItem('token');
   const token = JSON.parse(tokenString);
   return token;
-}
+};
 
 const saveToken = (token) => {
-  localStorage.setItem("token", JSON.stringify(token));
-}
+  localStorage.setItem('token', JSON.stringify(token));
+};
 const saveIsAdmin = (isAdmin) => {
-  localStorage.setItem("isAdmin", isAdmin);
-}
+  localStorage.setItem('isAdmin', isAdmin);
+};
 
 const getIsAdmin = () => {
-  const isAdminString = localStorage.getItem("isAdmin");
+  const isAdminString = localStorage.getItem('isAdmin');
   const isAdmin = JSON.parse(isAdminString);
   return isAdmin;
-}
+};
 
 export const AuthProvider = ({ children }) => {
-
   const [auth, setAuth] = useState({
-    isLoggedIn: getToken()? true : false,
+    isLoggedIn: getToken() ? true : false,
     isAdmin: getIsAdmin(),
   });
 
   const setAuthr = (token, isAdmin = false) => {
-    if(token != undefined && isAdmin != undefined) {saveToken(token); saveIsAdmin(isAdmin);}
-    setAuth({isLoggedIn: getToken()? true : false,
-      isAdmin: getIsAdmin(),});
-    
-  }
+    if (token !== undefined && isAdmin !== undefined) {
+      saveToken(token);
+      saveIsAdmin(isAdmin);
+    }
+    setAuth({ isLoggedIn: getToken() ? true : false, isAdmin: getIsAdmin() });
+  };
 
   return <AuthContext.Provider value={{ auth, setAuthr }}>{children}</AuthContext.Provider>;
 };
