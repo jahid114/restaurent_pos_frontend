cd63ca57c6bbf42a9e0c0033987dc8831405a5d3
matin <admatinm@gmail.com>
2022-11-05 17:01:00 +0600

    implement create-order facility

--- src/component/Order/CreateOrderForm.js
+++ src/component/Order/CreateOrderForm.js
@@ -2,13 +2,97 @@
 import '../../pages/login_page/login.css';
 import './formStyle.css';
 import { useNavigate } from 'react-router-dom';
+import config from '../../config';
+import useAuth from '../../hooks/UseAuth';
+import { useState, useEffect } from 'react';
+import Item from './Item';
+import OrderedItem from './OrderedItem';
 
+const processItemList = (response, navigate, setAuth, setItemsList) => {
+  if (response.status === 'success') {
+    setItemsList(response.data.items);
+  } else {
+    localStorage.clear();
+    setAuth();
+    navigate('/login');
+  }
+};
+let count = 0;
 const CreateOrderForm = () => {
+  const [itemList, setItemsList] = useState([]);
+  const [orderedItemList, setOrderedItemList] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const addToOrderedItemList = (itemId) => {
+    let o = itemList.find((e) => e._id === itemId);
+    let orderedItem = Object.assign({}, o);
+    orderedItem.qty = 1;
+    orderedItem.unitPrice = orderedItem.price;
+    orderedItem.serial = count++;
+    setOrderedItemList((prevState) => [...prevState, orderedItem]);
+  };
+
+  const modify = (itemId, offset) => {
+    let orderedItem = { ...orderedItemList.find((e) => e._id === itemId) };
+    orderedItem.qty = orderedItem.qty + offset;
+    orderedItem.price += offset * orderedItem.unitPrice;
+    if (orderedItem.qty < 0) orderedItem.qty = orderedItem.price = 0;
+    setOrderedItemList((prevState) => {
+      return [...prevState.filter((item) => item._id != itemId), orderedItem].sort((f, s) => f.serial - s.serial);
+    });
+  };
+
+  const authentication = useAuth();
+
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/home/orders');
+    fetch(config.apiurl + '/orders/create-order', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: 'Bearer ' + authentication.auth.token,
+      },
+      body: JSON.stringify({
+        totalPrice,
+        items: orderedItemList.map((item) => ({
+          item: item._id,
+          quantity: item.qty,
+        })),
+        status: 'unpaid',
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 'Success') {
+          alert('order added to the list');
+          navigate('/home/orders');
+        }
+      })
+      .catch((err) => console.log('error', err));
   };
+
+  const deleteItem = (itemId) => {
+    setOrderedItemList((prev) => [...prev.filter((item) => item._id != itemId)]);
+  };
+
+  useEffect(() => {
+    fetch(config.apiurl + '/items', {
+      headers: {
+        Authorization: 'Bearer ' + authentication.auth.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        processItemList(res, navigate, authentication.setAuthr, setItemsList);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    setTotalPrice(orderedItemList.reduce((prev, cur) => prev + cur.price, 0));
+  }, [orderedItemList]);
+
   return (
     <div className='parent'>
       <div className='mainContent container'>
@@ -51,22 +135,16 @@ const CreateOrderForm = () => {
                   <th className='pt-1 pb-1'>Price</th>
                   <th className='pt-1 pb-1'></th>
                 </tr>
-                <tr>
-                  <td className='pt-1 pb-1'>1</td>
-                  <td className='pt-1 pb-1'>Hamburger grande</td>
-                  <td className='pt-1 pb-1'>$250</td>
-                  <td className='pt-2 pb-2'>
-                    <button className='In-order btn'>In Order</button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Hamburger grande</td>
-                  <td>$250</td>
-                  <td className='pt-2 pb-2'>
-                    <button className='add-order btn'>+</button>
-                  </td>
-                </tr>
+                {itemList.map((item, indx) => (
+                  <Item
+                    name={item.name}
+                    price={item.price}
+                    id={indx}
+                    _id={item._id}
+                    isInOrder={orderedItemList.find((e) => e._id === item._id) ? true : false}
+                    addToOrderedItemList={addToOrderedItemList}
+                  />
+                ))}
               </table>
             </div>
             <div className='row'>
@@ -104,8 +182,8 @@ const CreateOrderForm = () => {
           <div className='col-md-6'>
             <div className='row ml-1'>
               <div className='total-board col-sm-12'>
-                <h2>Total $1870.00</h2>
-                <p>9 Items in Order</p>
+                <h2>Total {totalPrice}</h2>
+                <p>{orderedItemList.reduce((prev, cur) => prev + cur.qty, 0)} Items in Order</p>
               </div>
             </div>
             <div className='row ml-1'>
@@ -117,7 +195,18 @@ const CreateOrderForm = () => {
                   <th className='pt-1 pb-1'>Total</th>
                   <th className='pt-1 pb-1'></th>
                 </tr>
-                <tr>
+                {orderedItemList.map((item) => (
+                  <OrderedItem
+                    _id={item._id}
+                    name={item.name}
+                    price={item.price}
+                    qty={item.qty}
+                    modify={modify}
+                    deleteItem={deleteItem}
+                  />
+                ))}
+
+                {/* <tr>
                   <td className='pt-1 pb-1'>HEINEKEN 750ML</td>
                   <td className='pt-1 pb-1'>3</td>
                   <td className='pt-1 pb-1'>
@@ -130,7 +219,7 @@ const CreateOrderForm = () => {
                       <i className='bi bi-trash' style={{ 'font-size': '1.5rem', color: 'red' }}></i>
                     </button>
                   </td>
-                </tr>
+                </tr> */}
               </table>
             </div>
             <div className='table row'>
